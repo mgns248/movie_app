57791aff83d858e0b90d7ee5ff20bf146ca155c8
mgns248 <magnus.brix@gmx.de>
2025-08-07 17:06:12 +0200

    Startseite fertig

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,11 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, FlatList } from 'react-native'
 import React from 'react'
+import { images } from '@/constants/images';
+import MovieCard from '@/components/MovieCard';
+import useFetch from '@/services/useFetch';
+import { fetchMovies } from '@/services/api';
 
 const Search = () => {
+
+  const { 
+    data: movies, 
+    loading: moviesLoading, 
+    error: moviesError 
+  } = useFetch(() => fetchMovies({
+    query: ''
+  }));
+
   return (
-    <View>
-      <Text>search</Text>
-    </View>
-  )
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover" />
+      <FlatList 
+      data={movies} 
+      renderItem={({ item }) => <MovieCard { ...item} /> } />
+    </View> 
+  );
 }
 export default Search;
